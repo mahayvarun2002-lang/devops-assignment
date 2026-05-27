f87c28d1bc08c396869564efcc4953fc10246a85
Varun <varun@example.com>
2026-05-27 21:51:00 +1000

    Fix ESLint quotes error

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,17 @@
 module.exports = {
-    "env": {
-        "node": true,
-        "commonjs": true,
-        "es2021": true,
-        "jest": true
+    'env': {
+        'node': true,
+        'commonjs': true,
+        'es2021': true,
+        'jest': true
     },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "ecmaVersion": 12
+    'extends': 'eslint:recommended',
+    'parserOptions': {
+        'ecmaVersion': 12
     },
-    "rules": {
-        "no-unused-vars": "warn",
-        "semi": ["error", "always"],
-        "quotes": ["error", "single"]
+    'rules': {
+        'no-unused-vars': 'warn',
+        'semi': ['error', 'always'],
+        'quotes': ['error', 'single']
     }
 };
